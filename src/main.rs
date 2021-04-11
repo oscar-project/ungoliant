15e14bf71e3ee4e4be5521740c59f918b317c1bd
Pedro J. Ortiz <pedro@pjortiz.com>
2021-04-11 20:48:22 +0200

    added logging and removed decompress function for now

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-//extern crate reqwest;
-extern crate flate2;
-
 use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
-use flate2::bufread::MultiGzDecoder;
+//use flate2::bufread::MultiGzDecoder;
 
 fn main() {
     let base_url = "https://commoncrawl.s3.amazonaws.com/";
@@ -14,19 +11,49 @@ fn main() {
     let f = BufReader::new(f);
     let mut i = 0;
 
+    let mut err_file = File::create("errors.txt").expect("failed to create error file");
+    let mut log_file = File::create("log.txt").expect("failed to create log file");
+
     for line in f.lines() {
         let line = line.unwrap();
         let target = format!("{}{}", base_url, line);
-        let res = reqwest::blocking::get(&target).unwrap();
-        println!("Crawling {:?}", line);
 
-        
+        let res = reqwest::blocking::get(&target);
+        let res = match res {
+            Ok(resp) => resp,
+            Err(error) => {
+                write!(err_file, "Problem downloading the file: {} => {}\n", line, i).unwrap();
+                write!(err_file, "Error: {}\n", error).unwrap();
+                i += 1;
+                continue;
+            },
+        };
+        println!("Crawling {} to file {}.txt.gz", line, i);
+        write!(log_file, "Crawling {} to file {}.txt.gz\n", target, i).unwrap();
 
-        let mut out = File::create(format!("result/{}.txt", i.to_string())).expect("failed to create file");
-        //res.copy_to(&mut out).unwrap();
-        let buf = BufReader::new(res);
-        let mut gz = MultiGzDecoder::new(buf);
-        io::copy(&mut gz, &mut out).unwrap();
+        let out = File::create(format!("result/{}.txt.gz", i.to_string()));
+        let mut out = match out{
+            Ok(file) => file,
+            Err(error) => {
+                write!(err_file, "Problem creating the file: {}.txt.gz\n", i).unwrap();
+                write!(err_file, "Error: {}\n", error).unwrap();
+                i += 1;
+                continue;
+            },
+        };
+        let mut buf = BufReader::new(res);
+        match io::copy(&mut buf, &mut out) {
+            Ok(_) => (),
+            Err(error) => {
+                write!(err_file, "Problem writing to file: {}.txt.gz\n", i).unwrap();
+                write!(err_file, "Error: {}\n", error).unwrap();
+                i += 1;
+                continue;
+            },
+        };
+        // let buf = BufReader::new(res);
+        // let mut gz = MultiGzDecoder::new(buf);
+        // io::copy(&mut gz, &mut out).unwrap();
         i += 1;
     }
 }
