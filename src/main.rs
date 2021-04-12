b1e2727999e7c0cca64a07b1ab5f2cb85bf14881
Julien Abadji <julien.abadji@inria.fr>
2021-04-12 17:47:32 +0200

    fix: begin refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use std::{fs::File, path::{PathBuf}};
 use std::io;
-use std::io::BufReader;
 use std::io::prelude::*;
-use std::fs::File;
+use std::io::BufReader;
+use structopt::StructOpt;
 //use flate2::bufread::MultiGzDecoder;
 
-fn main() {
-    let base_url = "https://commoncrawl.s3.amazonaws.com/";
+const BASE_URL: &str = "https://commoncrawl.s3.amazonaws.com/";
 
-    let f = File::open("test.wet.paths").unwrap();
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ungoliant", about = "A mysterious project named after a spider that consumes everything 🕷️.")]
+struct UngoliantCli {
+    #[structopt(help="paths to download, ending in wet.paths.")]
+    file: PathBuf,
+}
+
+fn main() -> Result<(), std::io::Error> {
+
+    let opt = UngoliantCli::from_args();
+
+    let f = File::open(opt.file)?;
     let f = BufReader::new(f);
     let mut i = 0;
 
@@ -16,30 +27,35 @@ fn main() {
 
     for line in f.lines() {
         let line = line.unwrap();
-        let target = format!("{}{}", base_url, line);
+        let target = format!("{}{}", BASE_URL, line);
 
         let res = reqwest::blocking::get(&target);
         let res = match res {
             Ok(resp) => resp,
             Err(error) => {
-                write!(err_file, "Problem downloading the file: {} => {}\n", line, i).unwrap();
+                write!(
+                    err_file,
+                    "Problem downloading the file: {} => {}\n",
+                    line, i
+                )
+                .unwrap();
                 write!(err_file, "Error: {}\n", error).unwrap();
                 i += 1;
                 continue;
-            },
+            }
         };
         println!("Crawling {} to file {}.txt.gz", line, i);
         write!(log_file, "Crawling {} to file {}.txt.gz\n", target, i).unwrap();
 
         let out = File::create(format!("result/{}.txt.gz", i.to_string()));
-        let mut out = match out{
+        let mut out = match out {
             Ok(file) => file,
             Err(error) => {
                 write!(err_file, "Problem creating the file: {}.txt.gz\n", i).unwrap();
                 write!(err_file, "Error: {}\n", error).unwrap();
                 i += 1;
                 continue;
-            },
+            }
         };
         let mut buf = BufReader::new(res);
         match io::copy(&mut buf, &mut out) {
@@ -49,11 +65,13 @@ fn main() {
                 write!(err_file, "Error: {}\n", error).unwrap();
                 i += 1;
                 continue;
-            },
+            }
         };
         // let buf = BufReader::new(res);
         // let mut gz = MultiGzDecoder::new(buf);
         // io::copy(&mut gz, &mut out).unwrap();
         i += 1;
     }
+
+    Ok(())
 }
