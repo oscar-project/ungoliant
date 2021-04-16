ffe801bf59c21be71ab8a5ba79a87bd0cf776aeb
uj <julien.abadji@inria.fr>
2021-04-16 14:54:24 +0200

    feat(warc): add iterator over warc record bodies

--- src/warc.rs
+++ src/warc.rs
@@ -1,13 +1,14 @@
 use std::{
     fs::File,
     io::{BufReader, BufWriter, Write},
+    path::Path,
     thread::current,
 };
 
-use flate2::bufread::GzDecoder;
+use flate2::bufread::{GzDecoder, MultiGzDecoder};
 use itertools::Itertools;
 // use libflate::gzip::Decoder as GzipReader;
-use libflate::gzip::MultiDecoder as GzipReader;
+use libflate::gzip::MultiDecoder;
 use std::io::BufRead;
 use std::path::PathBuf;
 use warc::header::WarcHeader;
@@ -29,23 +30,35 @@ impl From<std::io::Error> for Error {
     }
 }
 
-/// strips warcs (for now)
-/// should/will return an iterator over record
-/// in order to be easily pipelined.
-pub fn strip_warc(path: &PathBuf) -> Result<(), Error> {
-    let gzip_file = File::open(path)?;
-    let gzip_stream = GzipReader::new(gzip_file)?;
+pub struct Wet<T> {
+    reader: WarcReader<T>,
+}
 
-    // we use a different reader from the default one in the warc crate to
-    // manage multipart gzipped content.
-    let reader = BufReader::with_capacity(10 * MB, gzip_stream);
+impl Wet<BufReader<MultiDecoder<File>>> {
+    pub fn from_path_gzip<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
+        let gzip_file = File::open(path)?;
+        let gzip_stream = MultiDecoder::new(gzip_file)?;
 
-    let gzd = WarcReader::new(reader);
-    for record in gzd {
-        match record {
-            Ok(r) => println!("{:?}", String::from_utf8_lossy(&r.body)),
-            Err(e) => eprintln!("{:?}", e),
+        // we use a different reader from the default one in the warc crate to
+        // manage multipart gzipped content.
+        let bufreader = BufReader::with_capacity(100 * MB, gzip_stream);
+
+        let reader = WarcReader::new(bufreader);
+
+        Ok(Self { reader })
+    }
+}
+
+impl<R: BufRead> Iterator for Wet<R> {
+    type Item = Result<String, warc::Error>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(n) = self.reader.next() {
+            match n {
+                Ok(record) => Some(Ok(String::from_utf8_lossy(&record.body).to_string())),
+                Err(e) => Some(Err(e)),
+            }
+        } else {
+            None
         }
     }
-    Ok(())
 }
