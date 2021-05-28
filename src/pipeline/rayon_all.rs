805e2c37f5634580a99b78faa392906ebf70f5cc
uj <julien.abadji@inria.fr>
2021-05-28 14:41:19 +0200

    feat(metadata): remove non-grouped code
    bench shows it's slower

--- src/pipeline/rayon_all.rs
+++ src/pipeline/rayon_all.rs
@@ -223,53 +223,35 @@ impl Pipeline<()> for RayonAll {
             }
 
             // group by lang to limit number of writes.
-            if GROUPING {
-                let mut sentences_to_write: HashMap<&'static str, String> = HashMap::new();
-                let mut metadata_to_write: HashMap<&'static str, Vec<Metadata>> = HashMap::new();
-                for (s, l, m) in shard_results.into_iter().flatten() {
-                    sentences_to_write
-                        .entry(l)
-                        .and_modify(|v| *v += &s)
-                        .or_insert(s);
+            let mut sentences_to_write: HashMap<&'static str, String> = HashMap::new();
+            let mut metadata_to_write: HashMap<&'static str, Vec<Metadata>> = HashMap::new();
+            for (s, l, m) in shard_results.into_iter().flatten() {
+                sentences_to_write
+                    .entry(l)
+                    .and_modify(|v| *v += &s)
+                    .or_insert(s);
 
-                    match metadata_to_write.get_mut(l) {
-                        Some(meta) => meta.push(m),
-                        None => {
-                            metadata_to_write.insert(l, vec![m]);
-                        }
-                    };
-                }
-
-                for lang in sentences_to_write.keys() {
-                    let mut fd = langfiles.get(lang).unwrap();
-                    let mut fd_meta = meta_files.get(lang).unwrap();
-
-                    fd.write_all(sentences_to_write.get(lang).unwrap().as_bytes())
-                        .unwrap();
-                    fd_meta
-                        .write_all(
-                            serde_json::to_string_pretty(metadata_to_write.get(lang).unwrap())
-                                .unwrap()
-                                .as_bytes(),
-                        )
-                        .unwrap();
-                }
-            } else {
-                //without grouping
-                for record in shard_results {
-                    for (sentences, lang, meta) in record {
-                        match langfiles.get(lang) {
-                            Some(mut fd) => fd.write_all(sentences.as_bytes()).unwrap(),
-                            None => error!("could not write to {} file", lang),
-                        }
-                        match meta_files.get(lang) {
-                            Some(mut fd) => fd
-                                .write_all(serde_json::to_string_pretty(&meta).unwrap().as_bytes())
-                                .unwrap(),
-                            None => error!("could not write to {} file", lang),
-                        }
+                match metadata_to_write.get_mut(l) {
+                    Some(meta) => meta.push(m),
+                    None => {
+                        metadata_to_write.insert(l, vec![m]);
                     }
-                }
+                };
+            }
+
+            for lang in sentences_to_write.keys() {
+                let mut fd = langfiles.get(lang).unwrap();
+                let mut fd_meta = meta_files.get(lang).unwrap();
+
+                fd.write_all(sentences_to_write.get(lang).unwrap().as_bytes())
+                    .unwrap();
+                fd_meta
+                    .write_all(
+                        serde_json::to_string_pretty(metadata_to_write.get(lang).unwrap())
+                            .unwrap()
+                            .as_bytes(),
+                    )
+                    .unwrap();
             }
         });
 
