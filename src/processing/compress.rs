53e368384ec73188fd42c9eea3e1171aadc3fe2e
uj <julien.abadji@inria.fr>
2021-07-28 11:28:46 +0200

    feat(compress): change return type, add doc

--- src/processing/compress.rs
+++ src/processing/compress.rs
@@ -11,32 +11,33 @@ use crate::error::Error;
 use log::{error, info};
 // use flate2::Compresion;
 
-// fn compress_lang(dst: &Path, lang: &'static str, reader: Reader) {
-//     let mut filepath = dst.clone();
-//     filepath.push(format!("{}.txt.gz", )
-//     let gz_writer = GzEncoder::new();
-// }
-pub fn compress_corpus(src: &Path, dst: &Path) -> Result<(), Error> {
+/// Compress a whole corpus using concurrently.
+///
+/// files in `src` will be kept (contrary to `gzip`'s behaviour).
+///
+/// Returns either a potentially empty vector of failed compressions, or an error related to directory reading/listing
+pub fn compress_corpus(src: &Path, dst: &Path) -> Result<Vec<Error>, Error> {
+    // There should be an easier way to do that.
     let files_to_compress: Result<Vec<_>, std::io::Error> = std::fs::read_dir(src)?.collect();
     let files_to_compress: Vec<PathBuf> =
         files_to_compress?.into_iter().map(|x| x.path()).collect();
     let files_to_compress = files_to_compress.into_par_iter();
+
+    // construct vector of errors
     let errors: Vec<Error> = files_to_compress
-        .filter_map(|filepath| match compress_file(&filepath, dst) {
-            Ok(_) => None,
-            Err(e) => Some(e),
-        })
+        .filter_map(|filepath| compress_file(&filepath, dst).err())
         .collect();
 
     if !errors.is_empty() {
-        for error in errors {
+        for error in &errors {
             error!("{:?}", error);
-            println!("{:?}", error);
         }
-    }
-    Ok(())
+    };
+
+    Ok(Vec::new())
 }
 
+/// compress a single file
 fn compress_file(path: &Path, dst: &Path) -> Result<(), Error> {
     let src = File::open(path)?;
     let b = BufReader::new(src);
