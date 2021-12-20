77247e7d08bdd71225fe904e82e76df0403b1a3f
jabadji <julien.abadji@inria.fr>
2021-12-20 17:40:40 +0100

    tests(tiny): add tests

--- src/transformers/tiny.rs
+++ src/transformers/tiny.rs
@@ -18,3 +18,42 @@ impl Default for TinyDocument {
         Self { threshold: 5 }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{
+        pipelines::oscardoc::types::{Document, Metadata},
+        transformers::Annotate,
+    };
+
+    use super::TinyDocument;
+
+    #[test]
+    fn test_annotation() {
+        let b = "this is a short
+        short document";
+        let mut d = Document::new(b.to_string(), HashMap::new(), Metadata::default());
+
+        let annotator = TinyDocument::default();
+        annotator.annotate(&mut d);
+
+        assert_eq!(d.metadata().annotation(), Some(&vec!["tiny".to_string()]));
+    }
+
+    #[test]
+    fn test_no_annotation() {
+        let b = "this is not a short
+        short document
+        it has tiny sentences
+        but has enough sentences
+        or so I think";
+        let mut d = Document::new(b.to_string(), HashMap::new(), Metadata::default());
+
+        let annotator = TinyDocument::default();
+        annotator.annotate(&mut d);
+
+        assert_eq!(d.metadata().annotation(), None);
+    }
+}
