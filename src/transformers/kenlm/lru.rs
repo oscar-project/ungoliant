a84bc530faf4cb47d2abe5760e39fb62c4e403f1
jabadji <julien.abadji@inria.fr>
2022-11-29 11:26:19 +0100

    simplify models struct, add LRU later on

--- src/transformers/kenlm/lru.rs
+++ src/transformers/kenlm/lru.rs
@@ -1,70 +1,34 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
+
+use log::{debug, error};
 
 use super::{adult_content::AdultDetectorBuilder, AdultDetector};
 
-enum ModelState {
-    Loaded((AdultDetector, AdultDetectorBuilder)),
-    NotLoaded(AdultDetectorBuilder),
-}
 struct Models {
-    model_list: HashMap<String, Option<AdultDetector>>,
-    cache: HashMap<String, usize>, // Lang -> Age
-    current_age: usize,
-    max_models: usize,
+    models: HashMap<String, AdultDetector>,
+    builders: HashMap<String, AdultDetectorBuilder>,
+    //cache: Vec<String>,
+    //cache_len: usize,
 }
 
 impl Models {
-    /// oldest == min age
-    fn remove_oldest(&mut self) {
-        if let Some((oldest, _)) = self
-            .cache
-            .iter()
-            .min_by(|(a, age_a), (b, age_b)| age_a.cmp(age_b))
-        {
-            self.cache.remove(&*oldest);
-
-            // unload model
-            if let Some(detector) = self.model_list.get_mut(&*oldest) {
-                *detector = None;
-            }
+    /// Get model of provided language. Attempts to build it if it is not yet instantiated
+    pub fn get(&mut self, lang: &str) -> Option<&AdultDetector> {
+        // two step to avoid immutable+mutable borrow
+        if !self.models.contains_key(lang) {
+            debug!("building KenLM {lang}");
+            self.load(lang);
         }
+        self.models.get(lang)
     }
-    pub fn get(&mut self, lang: &str) -> &Option<AdultDetector> {
-        let entry = self.model_list.get(lang);
 
-        if let Some(entry) = entry {
-            //bump model to most recently used
-            if let Some(cache_entry) = self.cache.get_mut(lang) {
-                *cache_entry = self.current_age;
-                self.current_age += 1;
-            }
+    fn unload(&mut self, lang: &str) {
+        self.models.remove(lang);
+    }
+
+    fn load(&mut self, lang: &str) {
+        if let Some(builder) = self.builders.get(lang) {
+            self.models.insert(lang.to_owned(), builder.build());
         }
-
-        todo!()
-        // match self.model_list.get_mut(lang) {
-        //     Some(model) => {
-        //         // bump model to most recently used
-        //         if let Some(cache_entry) = self.cache.get_mut(lang) {
-        //             *cache_entry = self.current_age;
-        //             self.current_age += 1;
-        //         }
-        //         model
-        //     }
-        //     e => {
-        //         // we know it's none
-        //         if self.cache.len() >= self.max_models {
-        //             self.remove_oldest(); // make place
-        //         }
-
-        //         // load model
-        //         // TODO find a way to
-        //         e = Some(AdultDetector::default());
-
-        //         self.cache.insert(lang.to_string(), self.current_age);
-        //         self.current_age += 1;
-
-        //         e
-        //     }
-        // }
     }
 }
